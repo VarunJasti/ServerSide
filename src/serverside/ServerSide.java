6f77e939b1292fec68f2640b1a04c4f6c43639db
indiangravy <varunjasti03@gmail.com>
2021-05-10 17:21:17 -0700

    Update ServerSide.java

--- src/serverside/ServerSide.java
+++ src/serverside/ServerSide.java
@@ -15,12 +15,13 @@ public class ServerSide {
         int port = Integer.parseInt(args[0]);
         try {
             ServerSocket server = new ServerSocket(port);
-            System.out.println("Server Running on" + port);
+            System.out.println("Server Running on " + port);
             while (true) {
                 Socket client = server.accept();
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                 String input = in.readLine();
+                System.out.println(input);
                 out.println(input);
             }
         } catch (Exception e) {}
