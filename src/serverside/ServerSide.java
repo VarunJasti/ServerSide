753f4db4bddfeb1afdca9d807d7feb762cd2bc2f
indiangravy <varunjasti03@gmail.com>
2021-06-01 19:58:28 -0700

    This code doesn't work

--- src/serverside/ServerSide.java
+++ src/serverside/ServerSide.java
@@ -67,65 +67,73 @@ public class ServerSide {
     }
 
     public static void playPoker() {
-        Deck d = new Deck();
-        for (User u : clients) {
-            u.setHand(d.deal(2));
-            u.sendHandToClient();
-        }
-        round();
-        ArrayList<Card> community = d.deal(3);
-        String cards = "community|";
-        for (Card c : community) {
-            cards += c.toStringInt() + "|";
-        }
-        for (User u : clients) {
-            u.getOut().println(cards);
-        }
-        round();
-        community.add(d.deal(1).get(0));
-        for (User u : clients) {
-            u.getOut().println("1community|" + community.get(3).toStringInt() + "|");
-        }
-        round();
-        community.add(d.deal(1).get(0));
-        for (User u : clients) {
-            u.getOut().println("2community|" + community.get(4).toStringInt() + "|");
-        }
-        for (User u : clients) {
-            for (int i = 0; i < clients.size(); i++) {
-                clients.get(i).getOut().println("usercard." + u.getUser() + ".|" + u.getHand().get(0).toStringInt() + "|" + u.getHand().get(1).toStringInt() + "|");
+        while (true) {
+            bet = 0;
+            Deck d = new Deck();
+            for (User u : clients) {
+                u.setHand(d.deal(2));
+                u.sendHandToClient();
             }
-        }
-        ArrayList<Hand> hands = new ArrayList<>();
-        ArrayList<Hand> bestHands = new ArrayList<>();
-        for (User u : clients) {
-            hands.clear();
-            combinations(community, u.getHand(), hands);
-            Collections.sort(hands);
-            hands.get(0).setUser(u);
-            bestHands.add(hands.get(0));
-        }
-        Collections.sort(bestHands);
-        ArrayList<Hand> winners = new ArrayList<>();
-        boolean check = true;
-        int i = 0;
-        while (check && i < bestHands.size()) {
-            winners.add(bestHands.get(i));
-            if (bestHands.get(i).compareTo(bestHands.get(i + 1)) == 0) {
-                i++;
-            } else {
-                check = false;
+            round();
+            ArrayList<Card> community = d.deal(3);
+            String cards = "community|";
+            for (Card c : community) {
+                cards += c.toStringInt() + "|";
             }
-        }
-        double pot = 0;
-        for (User u : clients) {
-            pot += u.getBet();
-        }
-        pot = pot / (winners.size() * 1.0);
-        System.out.println(pot);
-        for (Hand hand : winners) {
-            hand.getUser().getOut().println("win," + pot);
-            System.out.println("Winner: " + hand.getUser().getUser() + " --> " + hand.getCards());
+            for (User u : clients) {
+                u.getOut().println(cards);
+            }
+            round();
+            community.add(d.deal(1).get(0));
+            for (User u : clients) {
+                u.getOut().println("1community|" + community.get(3).toStringInt() + "|");
+            }
+            round();
+            community.add(d.deal(1).get(0));
+            for (User u : clients) {
+                u.getOut().println("2community|" + community.get(4).toStringInt() + "|");
+            }
+            for (User u : clients) {
+                for (int i = 0; i < clients.size(); i++) {
+                    clients.get(i).getOut().println("usercard." + u.getUser() + ".|" + u.getHand().get(0).toStringInt() + "|" + u.getHand().get(1).toStringInt() + "|");
+                }
+            }
+            ArrayList<Hand> hands = new ArrayList<>();
+            ArrayList<Hand> bestHands = new ArrayList<>();
+            for (User u : clients) {
+                hands.clear();
+                combinations(community, u.getHand(), hands);
+                Collections.sort(hands);
+                hands.get(0).setUser(u);
+                bestHands.add(hands.get(0));
+            }
+            Collections.sort(bestHands);
+            ArrayList<Hand> winners = new ArrayList<>();
+            boolean check = true;
+            int i = 0;
+            while (check && i < bestHands.size()) {
+                winners.add(bestHands.get(i));
+                if (bestHands.get(i).compareTo(bestHands.get(i + 1)) == 0) {
+                    i++;
+                } else {
+                    check = false;
+                }
+            }
+            double pot = 0;
+            for (User u : clients) {
+                pot += u.getBet();
+            }
+            pot = pot / (winners.size() * 1.0);
+            System.out.println(pot);
+            for (Hand hand : winners) {
+                hand.getUser().getOut().println("win," + pot);
+                System.out.println("Winner: " + hand.getUser().getUser() + " --> " + hand.getCards());
+            }
+            if (playing) {
+                User temp = clients.get(0);
+                clients.remove(0);
+                clients.add(temp);
+            }
         }
     }
 
@@ -152,7 +160,8 @@ public class ServerSide {
     }
 
     public static void round() {
-        for (User u : clients) {
+        for (int i = 0; i < clients.size(); i++) {
+            User u = clients.get(i);
             if (!u.isFold()) {
                 u.getOut().printf("bet,%.2f%n", bet);
                 try {
@@ -166,17 +175,19 @@ public class ServerSide {
                         }
                     } else if (input.equals("fold")) {
                         u.setFold(true);
-                        for (int i = 0; i < clients.size(); i++) {
-                            clients.get(i).getOut().println("userfold," + u.getUser());
+                        for (int j = 0; j < clients.size(); j++) {
+                            clients.get(j).getOut().println("userfold," + u.getUser());
                         }
                     }
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
-                    //Something for user disconnecting
+                    disconnect(u);
+                    return;
                 }
             }
         }
-        for (User u : clients) {
+        for (int i = 0; i < clients.size(); i++) {
+            User u = clients.get(i);
             if (!u.isFold() && u.getBet() < bet) {
                 u.getOut().printf("call,%.2f%n", bet);
                 try {
@@ -190,16 +201,26 @@ public class ServerSide {
                         }
                     } else if (input.equals("fold")) {
                         u.setFold(true);
-                        for (int i = 0; i < clients.size(); i++) {
-                            clients.get(i).getOut().println("userfold," + u.getUser());
+                        for (int j = 0; j < clients.size(); j++) {
+                            clients.get(j).getOut().println("userfold," + u.getUser());
                         }
                     }
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
-                    //Something for user disconnecting
+                    disconnect(u);
+                    return;
                 }
             }
         }
+    }
+
+    private static void disconnect(User u) {
+        System.out.println(u.getUser() + " disconnected");
+        for (int i = 0; i < clients.size(); i++) {
+            clients.get(i).getOut().println("endgame");
+        }
+        playing = false;
+        clients.clear();
     }
 
     public static void setPlaying(boolean play) {
